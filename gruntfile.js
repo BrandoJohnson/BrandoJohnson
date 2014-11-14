804b9fa538dce43f64d26a363fd911d7883503f8
brandojohnson <brandon@rjohnsononline.com>
2014-11-13 22:40:35 -0600

    Re-installed node, grunt, and dependancies. Added to VCS.

--- gruntfile.js
+++ gruntfile.js
@@ -1,31 +1,13 @@
 module.exports = function(grunt) {
     grunt.initConfig({
-        watch: {
-            styles: {
-                files: ['css/demo.css', 'css/icons.css', 'css/normalize.css', 'css/style3.css'], // which files to watch
-                tasks: ['cssmin'],
-                options: {
-                    nospawn: true,
-                    livereload: true
-                }
-            }
-        },
 
         watch: {
             styles: {
-                files: ['css/less/bootstrap.less', 'css/less/layout.less', 'css/less/typography.less', 'css/less/utility.less'], // which files to watch
+                files: ['css/less/bootstrap.less', 'css/less/layout.less', 'css/less/typography.less', 'css/less/ui.less', 'css/less/utility.less', 'css/less/variables.less'], // which files to watch
                 tasks: ['less'],
                 options: {
                     nospawn: true,
                     livereload: true
-                }
-            }
-        },
-
-        cssmin: {
-            combine: {
-                files: {
-                    'style.css': ['css/demo.css', 'css/icons.css', 'css/normalize.css', 'css/style3.css', 'css/less/bootstrap.less', 'css/less/layout.less', 'css/less/typography.less', 'css/less/utility.less']
                 }
             }
         },
@@ -38,16 +20,15 @@ module.exports = function(grunt) {
                     optimization: 2
                 },
                 files: {
-                    "style.css": "css/less/styles.less"
+                    // target.css file: source.less file
+                    "style.css": "css/less/style.less"
                 }
             }
         }
     });
 
-
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
     grunt.registerTask('default', ['watch']);
 };
