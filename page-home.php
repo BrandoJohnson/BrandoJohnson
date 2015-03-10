14f035da7602e908771e9187efabd71f40a4fc9f
brandojohnson <brandon@rjohnsononline.com>
2015-03-09 20:58:28 -0500

    Test

--- page-home.php
+++ page-home.php
@@ -15,7 +15,7 @@
     <div class="container">
         <header class="codrops-header">
             <h1><?php the_field('main_title'); ?> <span><?php the_field('headline'); ?></h1>
-            <p>Web Developer from Minneapolis MNNd. I own Public Service Websites and also do a fair amount of free lance development work. Feel free to get in touch!</p>
+            <p>Web Developer from Minneapolis MNN. I own Public Service Websites and also do a fair amount of free lance development work. Feel free to get in touch!</p>
             <nav class="codrops-demos">
                 <a href="<?php the_field('button1_link'); ?>"><?php the_field('button1'); ?></a>
                 <a href="<?php the_field('button2_link'); ?>"><?php the_field('button2'); ?></a>
