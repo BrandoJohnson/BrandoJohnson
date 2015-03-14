98926a9ef78db25e32779c54be4256cf4bc95ced
brandojohnson <brandon@rjohnsononline.com>
2015-03-13 23:16:10 -0500

    Page type - contact page.

---
+++ page-contact.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Template Name: My Custom Page
+*/
+
+get_header(); ?>
+
+<div id="content" class="site-content" role="main">
+    <?php /* Start the Loop */ ?>
+    <?php while ( have_posts() ) : the_post(); ?>
+        <?php the_content(); ?>
+    <?php endwhile; ?>
+</div><!-- #content .site-content -->
+
+<?php get_footer(); ?>
